6bce56b9cb367114ed4ab487e3ce7ac78f162b4b
Ahmednoor114 <60637755+Ahmednoor114@users.noreply.github.com>
2021-09-28 00:06:55 -0500

    Innitial

--- src/main/java/com/abdisg/theproblemwithscanner/ScannerTest.java
+++ src/main/java/com/abdisg/theproblemwithscanner/ScannerTest.java
@@ -5,10 +5,39 @@
  */
 package com.abdisg.theproblemwithscanner;
 
+import java.util.Scanner;
+
 /**
  *
  * @author ahmednoorabdi
  */
 public class ScannerTest {
+    public static void main(String[] args){
+        String name;
+        int age;
+        int numComputers;
+        String hometown;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.println("Please enter your name: ");
+        name = sc.nextLine();
+        System.out.println("Please enter your age: ");
+        age = sc.nextInt();
+        System.out.println("Please enter number of your computers: ");
+        numComputers = sc.nextInt();
+        
+        sc.nextLine();
+        System.out.println("Please enter your Hometown: ");
+        hometown = sc.nextLine();
+        
+        
+        System.out.println("You said: ");
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Number of Computers: " + numComputers);
+        System.out.println("Hometown: " + hometown);
+       
+    }
     
 }
